5f5182d916844c1aa2232d571f7216db77f099fe
Simi Antony <simi.tresa.antony@gmail.com>
2016-08-15 12:42:51 -0700

    added for to create accounts

--- controllers/accountsCtrl.js
+++ controllers/accountsCtrl.js
@@ -5,38 +5,59 @@ var conn                =  new jsforce.Connection();
 var mySDFC_Username     = 'simi.tresa.antony@gmail.com';
 var mySDFC_Password     = 'simiraj2010nDDMSAazy9Qbqn5uG1t7qwoI';
 var accounts            = [];
+var accessToken         ='';
+var instanceUrl          = '';
+
 genericCallback = function(error,result){
     if(error) {
         console.log('Error--> '+error);
     }
     else{
-        console.log('Result after connecting to SF ');
-        console.log(result);
+        console.log('Connected to SF ');
+        // get the access token and instance URL information.
+        accessToken = conn.accessToken;
+        instanceUrl = conn.instanceUrl;
+        console.log('instanceUrl'+conn.instanceUrl);
+        // logged in user properly
+        console.log("User ID: " + result.id);
+        console.log("Org ID: " + result.organizationId);
+        //console.log('sfSessionId = '+sfSessionId+', sessionURL = '+sessionURL);
     }   
 }
+ conn.login(mySDFC_Username,mySDFC_Password,genericCallback);
+               
 
-exports.accounts = function(req,res){
-    
-    async.waterfall([
-        function(callback) {
+exports.addAccount = function(req,res){
+    console.log('---------------Inside addAccount---------------');
+    console.log(req.body);
+    res.redirect('/accounts');
+}
+
+exports.displayAccounts = function(req,res){
+    console.log('Inside displayAccounts');
+   // async.waterfall([
+        /*function(callback) {
             conn.login(mySDFC_Username,mySDFC_Password,genericCallback);
             console.log('Connecting to SF DB');
+            //res.send('Connected successfully');
             setTimeout(function(){
                  callback(null); 
             },2000);    
         },
-        function(callback) {
+        function(callback) {*/
             conn.query('select id,name from Account limit 10',function(error,result){
             if(error){
                     console.log('Cannot fetch data'+error);
-            }else{  
-                    console.log(result.records);
-                    res.render('accounts',{
-                        title :"Top 10 Accounts in this org",
-                        accounts :result.records
-                     });
             }
-            });
+            //console.log(result.records);
+            res.render('accounts',{
+                title :"Top 10 Accounts in this org",
+                accounts :result.records
+                });
+            
+            
+            });/*
+
             //callback(accounts,null);
         }
         /*,
@@ -46,7 +67,7 @@ exports.accounts = function(req,res){
                         accounts :accountsResult
             });
         }*/
-    ]); 
+   // ]); 
    
 }
 
--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 console.log("Welcome to Intro to Node!");
 // Create an Express app
 // https://expressjs.com/en/starter/hello-world.html
-var express = require('express');
+var express = require('express'); 
 var jade = require('jade');
 var app = express();
+var bodyParser = require('body-parser');
 var homeCtrl = require('./controllers/homeCtrl') ; 
 var acctsCtrl = require('./controllers/accountsCtrl') ; 
+
  
- 
-app.set('views', './views');
+
+app.set('views', './views'); 
 app.set('view engine', 'jade');  
 app.use(express.static('public'));
 app.use(express.static('node_modules')); 
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 //--- check if the homeCtrl file is included----/
 console.log('the home control-->'+homeCtrl.home);  
   
 // Request response handling---
 app.get('/',homeCtrl.home); 
-app.get('/accounts',acctsCtrl.accounts); 
+app.get('/accounts',acctsCtrl.displayAccounts); 
+app.post('/addAccnt',acctsCtrl.addAccount); 
  
+
   
 // Start your Express app up on port 3000
 //---starting the server--------
